039c22ca421fea75801cf5a0fef13f806967a556
Amir Kaudinov <kaudinovamir@gmail.com>
2021-05-06 19:53:33 -0600

    -Updated API

--- src/api/DriversApi.js
+++ src/api/DriversApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class Api {
-    static getDrivers(){ //only can be called without instantiating the class itself
+    static getDrivers(){
         return axios.get('http://ergast.com/api/f1/2021/drivers.json').then(response => {
             return response.data.MRData.DriverTable.Drivers
         });
